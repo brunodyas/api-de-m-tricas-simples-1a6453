d63f85b581580bb12ee9034d04a79d6b3ae1761e
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 11:42:53 -0300

    feat: configuração estática
    intent: feature

--- obj/Debug/net8.0/api_de_m_tricas_simples_1a6453.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_de_m_tricas_simples_1a6453.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_de_m_tricas_simples_1a6453")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f517aac33009e92415925f06b579a6bdc3f8877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c66fe792e073b83d6b5e3bc87ab283cfe47ad42")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_de_m_tricas_simples_1a6453")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_de_m_tricas_simples_1a6453")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
